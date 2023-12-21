578e824944692e131ba58b31d5e1f6b51367e035
Megan Coningsby <meganconingsby@gmail.com>
2023-12-22 10:22:25 +1100

    Add user to vet appts index view, allow delete of vet appts, create edit page for appts (no update yet)

--- controllers/appts.js
+++ controllers/appts.js
@@ -4,8 +4,11 @@ const Vet = require('../models/vet');
 
 async function index(req, res) {
     const appts = await Appt.find({})
-        .populate('cat')
         .populate('vet')
+        .populate({
+            path: 'cat',
+            populate: { path: 'carer' },
+        })
         .sort({ date: -1 });
     res.render('appts/index', { title: 'All Vet Appts', appts });
 }
@@ -26,7 +29,51 @@ async function create(req, res) {
     }
 }
 
+async function edit(req, res) {
+    const appt = await Appt.findById(req.params.id);
+    const vets = await Vet.find({}).sort('name');
+    const currentVet = await Vet.find({
+        _id: appt.vet,
+    });
+    const currentVetName = currentVet[0].name;
+    let apptDate = appt.date;
+    apptDate = formatDate(apptDate);
+    res.render('appts/edit', {
+        title: `Edit Appt`,
+        appt,
+        vets,
+        currentVetName,
+        apptDate,
+        errorMsg: '',
+    });
+}
+
+async function remove(req, res) {
+    try {
+        const deleteAppt = await Appt.deleteOne({ _id: req.params.id });
+        res.redirect('/appts');
+    } catch (err) {
+        console.log(err);
+        res.redirect('/appts');
+    }
+}
+
 module.exports = {
     index,
     create,
+    edit,
+    delete: remove,
 };
+
+// Other functions to help with the above
+
+// Convert the date object into the right string format to be passed in to as a value for the edit page
+function formatDate(date) {
+    if (date !== null) {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+
+        return `${year}-${month}-${day}`;
+    }
+}
--- routes/appts.js
+++ routes/appts.js
@@ -5,5 +5,7 @@ const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 router.get('/appts', ensureLoggedIn, apptsController.index);
 router.post('/cats/:id/appts', ensureLoggedIn, apptsController.create);
+router.get('/appts/:id/edit', ensureLoggedIn, apptsController.edit);
+router.delete('/appts/:id', ensureLoggedIn, apptsController.delete);
 
 module.exports = router;
